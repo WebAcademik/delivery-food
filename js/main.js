f903b59dd091f084135ac14c9a6afb33e74dfe29
WebAcademik <59310911+WebAcademik@users.noreply.github.com>
2020-04-10 16:02:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const cardButton = document.querySelector('#card-button');
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cardButton.addEventListener('click', toggleModal);
+
+close.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle("modal-active");
+}
+
+new WOW().init();
